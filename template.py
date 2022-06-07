0aaaedfbb6e04fb385edd205c2015e38b8fdfb0b
Prathamesh Mahadeshwar <prathamesh.m@dgsl.biz>
2022-06-07 19:42:48 +0530

    add get data

--- template.py
+++ template.py
@@ -21,8 +21,8 @@ files = [
 
 for dir_ in dirs:
     os.makedirs(dir_, exist_ok=True)
-    # with open(os.path.join(dir_, ".gitkeep")) as f:
-    #     pass
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
+        pass
 
 for file_ in files:
     with open(file_, "w") as f:
